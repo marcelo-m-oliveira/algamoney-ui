ac0fdc7cd577c350eba229e991409dbb1e75d9fc
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-06-24 13:04:32 -0300

    Adicionando pagina de lançamentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import {PrimeNGConfig} from 'primeng/api';
-
-
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +7,26 @@ import {PrimeNGConfig} from 'primeng/api';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'algamoney-ui';
+  lancamentos = [
+    {
+      tipo: 'DESPESA', descricao: 'Compra de um celular', dataVencimento: '30/06/2022',
+      dataPagamento: null, valor: 1900, pessoa: 'SAMSUNG'
+    },
+    {
+      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/07/2022',
+      dataPagamento: null, valor: 3000, pessoa: 'IMAGE2'
+    },
+    {
+      tipo: 'DESPESA', descricao: 'Internet', dataVencimento: '27/06/2022',
+      dataPagamento: null, valor: 80, pessoa: 'Velho ponto net'
+    },
+    {
+      tipo: 'DEFESA', descricao: 'Namoro', dataVencimento: '12/06/2022',
+      dataPagamento: null, valor: 1200, pessoa: 'Kamila'
+    },
+    {
+      tipo: 'RECEITA', descricao: 'Socorro de Jesus', dataVencimento: '10/07/2022',
+      dataPagamento: null, valor: 4000, pessoa: 'Jesus'
+    },
+  ]
 }
