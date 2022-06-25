b879459e52e4d6e04da4091945cbd63d5af9350c
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-06-25 11:22:42 -0300

    Adicionando component de pessoas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { PRIMENG_IMPORTS } from './primeng-imports';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component'
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PessoasComponent } from './pessoas/pessoas.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentosPesquisaComponent
+    LancamentosPesquisaComponent,
+    NavbarComponent,
+    PessoasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pessoas/pessoas.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoas',
+  templateUrl: './pessoas.component.html',
+  styleUrls: ['./pessoas.component.css']
+})
+export class PessoasComponent {
+
+  pessoas = [
+    {
+      pessoa: 'Marcelo Oliveira', cidade: 'Salvador', estado: 'BA', status: true,
+    },
+    {
+      pessoa: 'Kamila Kelly', cidade: 'Feira de Santana', estado: 'BA', status: true,
+    },
+    {
+      pessoa: 'Batata', cidade: 'São Paulo', estado: 'SP', status: false,
+    },
+    {
+      pessoa: 'Amanda', cidade: 'Brasilia', estado: 'DF', status: true,
+    },
+    {
+      pessoa: 'Goku', cidade: 'Belo Horizonte', estado: 'BH', status: false,
+    },
+  ]
+}
