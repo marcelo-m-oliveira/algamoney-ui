a97b1ebc3cfcd7cc9e68962c71a7907b93a26879
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-06-26 11:33:34 -0300

    Adicionando formatações com pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { PRIMENG_IMPORTS } from './primeng-imports';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
@@ -19,7 +19,7 @@ import { PessoasComponent } from './pessoas/pessoas.component'
     BrowserAnimationsModule,
     PRIMENG_IMPORTS
   ],
-  providers: [],
+  providers: [{provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -8,24 +8,24 @@ export class LancamentosPesquisaComponent {
 
   lancamentos = [
     {
-      tipo: 'DESPESA', descricao: 'Compra de um celular', dataVencimento: '30/06/2022',
-      dataPagamento: null, valor: 1900, pessoa: 'SAMSUNG'
+      tipo: 'DESPESA', descricao: 'Compra de um celular', dataVencimento: new Date(2022, 6, 30),
+      dataPagamento: new Date(2022, 6, 10), valor: 1900, pessoa: 'SAMSUNG'
     },
     {
-      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/07/2022',
-      dataPagamento: null, valor: 3000, pessoa: 'IMAGE2'
+      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2022, 7 , 10),
+      dataPagamento: new Date(2022, 7, 1), valor: 3000, pessoa: 'IMAGE2'
     },
     {
-      tipo: 'DESPESA', descricao: 'Internet', dataVencimento: '27/06/2022',
-      dataPagamento: null, valor: 80, pessoa: 'Velho ponto net'
+      tipo: 'DESPESA', descricao: 'Internet', dataVencimento: new Date(2022, 6, 22),
+      dataPagamento: new Date(2022, 6, 10), valor: 80, pessoa: 'Velho ponto net'
     },
     {
-      tipo: 'DEFESA', descricao: 'Namoro', dataVencimento: '12/06/2022',
-      dataPagamento: null, valor: 1200, pessoa: 'Kamila'
+      tipo: 'DEFESA', descricao: 'Namoro', dataVencimento: new Date(2022, 6, 12),
+      dataPagamento: new Date(2022, 6, 11), valor: 1200, pessoa: 'Kamila'
     },
     {
-      tipo: 'RECEITA', descricao: 'Socorro de Jesus', dataVencimento: '10/07/2022',
-      dataPagamento: null, valor: 4000, pessoa: 'Jesus'
+      tipo: 'RECEITA', descricao: 'Socorro de Jesus', dataVencimento: new Date(2022, 7, 10),
+      dataPagamento: new Date(2022, 6, 30), valor: 4000, pessoa: 'Jesus'
     },
   ]
 }
