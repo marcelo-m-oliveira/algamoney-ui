d8b0968a0d28cd25ba1ca7669ff06186d96ea63b
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-07-28 07:04:54 -0300

    Pequenos ajustes

--- src/app/views/pessoas/pessoas-form/pessoas-form.component.ts
+++ src/app/views/pessoas/pessoas-form/pessoas-form.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pessoas-form',
   templateUrl: './pessoas-form.component.html',
 })
-export class PessoasFormComponent implements OnInit {
+export class PessoasFormComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
