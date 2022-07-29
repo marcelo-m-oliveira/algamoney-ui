031aca4b013f98921c7af911b157a36bfe618b26
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-07-29 12:58:45 -0300

    Exibindo dados na tabela de lancamentos via api;

--- front_end/src/app/app.module.ts
+++ front_end/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from "@angular/common/http"
 import { NgModule } from "@angular/core"
 
 import {CardModule} from "primeng/card";
@@ -9,6 +10,7 @@ import { CoreModule } from "./core/core.module"
 import { LancamentosModule } from "./views/lancamentos/lancamentos.module"
 import { PessoasModule } from "./views/pessoas/pessoas.module"
 import { AppComponent } from './app.component'
+import {LancamentoService} from "./views/lancamentos/lancamento.service";
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { AppComponent } from './app.component'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+
     CardModule,
 
     CoreModule,
@@ -26,7 +30,7 @@ import { AppComponent } from './app.component'
     PessoasModule,
 
   ],
-  providers: [],
+  providers: [LancamentoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- front_end/src/app/views/lancamentos/lancamentos/lancamentos.component.ts
+++ front_end/src/app/views/lancamentos/lancamentos/lancamentos.component.ts
@@ -1,31 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { LancamentoService } from "../lancamento.service"
 
 @Component({
   selector: 'app-lancamentos',
   templateUrl: './lancamentos.component.html',
 })
-export class LancamentosComponent {
+export class LancamentosComponent implements OnInit {
 
-  lancamentos = [
-    {
-      tipo: 'DESPESA', descricao: 'Compra de um celular', dataVencimento: new Date(2022, 6, 30),
-      dataPagamento: new Date(2022, 6, 10), valor: 1900, pessoa: 'SAMSUNG'
-    },
-    {
-      tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2022, 7 , 10),
-      dataPagamento: new Date(2022, 7, 1), valor: 3000, pessoa: 'IMAGE2'
-    },
-    {
-      tipo: 'DESPESA', descricao: 'Internet', dataVencimento: new Date(2022, 6, 22),
-      dataPagamento: new Date(2022, 6, 10), valor: 80, pessoa: 'Velho ponto net'
-    },
-    {
-      tipo: 'DESPESA', descricao: 'Namoro', dataVencimento: new Date(2022, 6, 12),
-      dataPagamento: new Date(2022, 6, 11), valor: 1200, pessoa: 'Kamila'
-    },
-    {
-      tipo: 'RECEITA', descricao: 'Socorro de Jesus', dataVencimento: new Date(2022, 7, 10),
-      dataPagamento: new Date(2022, 6, 30), valor: 4000, pessoa: 'Jesus'
-    },
-  ]
+  constructor(
+    private lancamentoService: LancamentoService
+  ) {}
+
+  lancamentos = []
+
+  ngOnInit(): void {
+    this.visualizar()
+  }
+
+  visualizar() {
+    this.lancamentoService.visualizar().then(lancamentos => this.lancamentos = lancamentos)
+  }
 }
