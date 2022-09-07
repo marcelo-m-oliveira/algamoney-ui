bc7948e5d6fe5f5b4d67f971ebac83eee80b0080
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-09-07 13:40:29 -0300

    Aplicando lazy loading para modulos;

--- front_end/src/app/views/pessoas/pessoas-routing.module.ts
+++ front_end/src/app/views/pessoas/pessoas-routing.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from "@angular/core"
-import { RouterModule, Routes } from "@angular/router"
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-import { PessoasComponent } from "./pessoas/pessoas.component"
-import { PessoasFormComponent } from "./pessoas-form/pessoas-form.component"
+import { PessoasComponent } from './pessoas/pessoas.component'
+import { PessoasFormComponent } from './pessoas-form/pessoas-form.component'
 
-import { SegurancaGuard } from "../seguranca/seguranca.guard"
+import { SegurancaGuard } from '../seguranca/seguranca.guard'
 
 const routes: Routes = [
   {
-    path: 'pessoas',
+    path: '',
     component: PessoasComponent,
     canActivate: [SegurancaGuard],
     data: { roles: ['ROLE_PESQUISAR_PESSOA'] }
   },
   {
-    path: 'pessoas/nova',
+    path: 'nova',
     component: PessoasFormComponent,
     canActivate: [SegurancaGuard],
     data: { roles: ['ROLE_CADASTRAR_PESSOA'] }
   },
   {
-    path: 'pessoas/:codigo',
+    path: ':codigo',
     component: PessoasFormComponent,
     canActivate: [SegurancaGuard],
     data: { roles: ['ROLE_CADASTRAR_PESSOA'] }
@@ -30,7 +30,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forChild(routes)
   ],
   exports: [
     RouterModule
