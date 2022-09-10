d3c1016a30fbfdbb11352d6915e452dcea9c7d75
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-09-10 16:59:45 -0300

    Criando funcionalidade e metodo para geração de relatorios por pessoa; Refatorando alguns metodos do LancamentoResource;

--- back_end/src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ back_end/src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -1,32 +1,59 @@
 package com.example.algamoney.api.service;
 
-import java.util.Optional;
+import java.io.InputStream;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.*;
 
+import com.example.algamoney.api.dto.LancamentoEstatisticaPessoa;
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.LancamentoRepository;
 import com.example.algamoney.api.repository.PessoaRepository;
 import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
 
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LancamentoService {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private LancamentoRepository lancamentoRepository;
+
+  public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
+    List<LancamentoEstatisticaPessoa> dados = lancamentoRepository.porPessoa(inicio, fim);
+    Map<String, Object> parametros = new HashMap<>();
+
+    parametros.put("DT_INICIO", Date.valueOf(inicio));
+    parametros.put("DT_FIM", Date.valueOf(fim));
+    parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+    InputStream inputStream = this.getClass().getResourceAsStream(
+      "/relatorios/lancamentos-por-pessoa.jasper"
+
+    );
+
+    JasperPrint jasperPrint = JasperFillManager.fillReport(inputStream, parametros,
+      new JRBeanCollectionDataSource(dados));
+
+    return JasperExportManager.exportReportToPdf(jasperPrint);
+  }
 
 	public Lancamento salvar(Lancamento lancamento) {
 		Optional<Pessoa> pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo());
 		if (pessoa.isEmpty() || pessoa.get().isInativo()) {
 			throw new PessoaInexistenteOuInativaException();
 		}
-		
+
 		return lancamentoRepository.save(lancamento);
 	}
 
@@ -58,6 +85,6 @@ public class LancamentoService {
 			throw new IllegalArgumentException();
 		} */
 		return lancamentoRepository.findById(codigo).orElseThrow(() -> new IllegalArgumentException());
-	}	
-	
+	}
+
 }
